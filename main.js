043069ad422c610bdd9eaec67136fac6cd11f93f
MrMubaroq <mubaroqahmad89@gmail.com>
2024-10-20 13:03:23 +0800

    commit ketiga

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let nilai = (prompt("input grade : "));
+
+if (nilai >= 90) {
+  console.log("Grade A");
+} else if (nilai >= 80) {
+  console.log("Grade B");
+} else if (nilai >= 70) {
+  console.log("Grade C");
+} else if (nilai >= 60) {
+  console.log("Grade D");
+} else if (nilai < 60) {
+  console.log("Grade E");
+};
